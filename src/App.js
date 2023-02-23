d1a62ff7da6263535eca59456772d7927754cd76
unknown <raghurengu005@gmail.com>
2023-02-23 23:40:03 +0530

    props and map function has been used. Styling is pending

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import {Card} from './components/Card';
+import data from './data';
+
+
+
+
 
 
 function App() {
+  const info = data.map(datas => {
+    return(
+      <Card 
+        img={datas.img}
+        rating={datas.rating}
+        revcount={datas.revcount}
+        country={datas.country}
+        title={datas.title}
+        price={datas.price}
+        />
+    )
+  })
+  
+
   return (
     <div>
         <Navbar />
         <Hero />
-        <Card />
+        {info}
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import star from '../images/star.png';
 
-export const Card = () => {
+
+
+export const Card = (props) => {
   return (
-    
+      <div className='card'>
+      <img src={props.img} className='zaf' />
+        <div className='stats'>
+          <img src={star} className='star' />
+          <span className='bold'>{props.rating}</span>
+          <span className='grey'>({props.revcount}) &middot; {props.country}</span>
+        </div>
+        <div>
+          <p>{props.title}</p>
+          <p><span className='bold'>From &#8377; {props.price}</span> / person</p>
+        </div>
+      </div>
     )
 }
+
+

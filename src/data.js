8a4d6ac01a826b6fad4b563cb3d57a6565a8b64e
unknown <raghurengu005@gmail.com>
2023-02-23 23:40:58 +0530

    props and map function has been used. Styling is pending

---
+++ src/data.js
@@ -0,0 +1,61 @@
+export default [
+    // {
+    //     img: "../images/zaf.png",
+    //     rating: "4.5",
+    //     revcount: "6",
+    //     country: "USA",
+    //     title: "Life lesson with Katie",
+    //     price: "400",
+    // },
+    {
+        img: "https://a0.muscache.com/im/pictures/miso/Hosting-605371928419351152/original/c8f48e8b-091d-47ea-85ac-b31bc2604bbb.jpeg?im_w=720",
+        rating: "4.1",
+        revcount: "60",
+        country: "India",
+        title: "Pimplad Naisk",
+        price: "4190",
+    },
+    {
+        img: "https://a0.muscache.com/im/pictures/miso/Hosting-598289084005563595/original/5037ecff-e315-4464-9c85-64503ae5b6ea.jpeg?im_w=720",
+        rating: "4.7",
+        revcount: "234",
+        country: "India",
+        title: "Lonavla",
+        price: "15000",
+    },
+    {
+        img: "https://a0.muscache.com/im/pictures/miso/Hosting-598289084005563595/original/5037ecff-e315-4464-9c85-64503ae5b6ea.jpeg?im_w=720",
+        rating: "4.3",
+        revcount: "12",
+        country: "Myanmar",
+        title: "Kashid",
+        price: "3800",
+    },
+    {
+        img: "https://a0.muscache.com/im/pictures/a1f47f50-5f72-4621-948a-4ac0a3dec950.jpg?im_w=720",
+        rating: "4.7",
+        revcount: "56",
+        country: "India",
+        title: "Savakar",
+        price: "4500",
+    },
+    {
+        img: "https://a0.muscache.com/im/pictures/miso/Hosting-46651958/original/ae60e9f7-5d52-454a-be04-4e608a7e3e21.jpeg?im_w=720",
+        rating: "4.2",
+        revcount: "90",
+        country: "India",
+        title: "colantar",
+        price: "4900",
+    },
+    {
+        img: "https://a0.muscache.com/im/pictures/miso/Hosting-825693346492766313/original/ec44a8f3-bf84-499c-9872-ad00c59b9979.png?im_w=720",
+        rating: "4.9",
+        revcount: "60",
+        country: "India",
+        title: "Chennai",
+        price: "4500",
+    },
+
+ 
+
+]
